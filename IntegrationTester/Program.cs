fd442b5be9c0d86c84df41a825ed1b6454b01644
John Rowley <johnrowleyster@gmail.com>
2015-11-03 16:42:12 -0800

    Initial commit!

---
+++ IntegrationTester/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTester
+{
+    public class Foo
+    {
+        public static string Do()
+        {
+            return "Bar";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Foo.Do());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IntegrationTesterTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntegrationTester;
+
+namespace IntegrationTesterTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Foo.Do(), "Bar");
+        }
+    }
+}
